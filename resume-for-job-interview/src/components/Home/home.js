c8954af9a39d3c8fbbd6dc7ecfaa5358d9c33599
Wannarat <wannarat.boonwong@gmail.com>
2022-09-19 17:06:02 +0700

    add-picture-profile

--- resume-for-job-interview/src/components/Home/home.js
+++ resume-for-job-interview/src/components/Home/home.js
@@ -1,13 +1,14 @@
-
 import './styles.css';
 function Home(){
     return(<>
     <div className="container-home">
-
-    <h1 className="description">Hi, I’m Wannarat</h1>
-    <div>
-        {/* <img src={require('../../asset/wannart-image.jpg')}/> */}
-    </div>
+        <div className="content">
+            <h1 className="topic-description">Hi, I’m Wannarat</h1>
+            <h3 className="detail-description">I am a graduated from Department of Computer science Faculty of science Chiangmai University. I am looking for a job in Front-End developer position, using my skills and knowledge to great effect for your company.</h3>
+        </div>
+        <div className="profile">
+            <img className="img-profile"src={require('../../asset/wannart-image.jpg')} alt="wannarat=pic"/>
+        </div>
     </div>
     </>)
 }
