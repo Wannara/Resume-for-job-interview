1ce81c4423f5adead2d7092b7dfc9bcef04982d7
Wannarat <wannarat.boonwong@gmail.com>
2022-09-21 15:26:23 +0700

    add-skills-component

--- resume-for-job-interview/src/App.js
+++ resume-for-job-interview/src/App.js
@@ -1,26 +1,27 @@
 // import logo from './logo.svg';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 import './App.css';
 import Education from './components/Education/education';
 import Experiences from './components/Experiences/experiences';
 import Home from './components/Home/home';
 import Navbar from './components/Navbar/Navbar';
+import Skills from './components/Skills/skills';
 
 function App() {
 
-  useEffect(()=>{
-    document.title = "Wannarat"
-  })
-  return (
-    <div className="App">
-
-      <Navbar></Navbar>
-      <div className="container">
-        <Home></Home>
-        <Education></Education>
-        <Experiences></Experiences>
-      </div>
-      {/* <header className="App-header">
+    useEffect(() => {
+        document.title = "Wannarat"
+    },[])
+    return (
+        <div className="App">
+            <Navbar></Navbar>
+            <div className="container">
+                <Home></Home>
+                <Education></Education>
+                <Experiences></Experiences>
+                <Skills></Skills>
+            </div>
+            {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -34,8 +35,8 @@ function App() {
           Learn React
         </a>
       </header> */}
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
---
+++ resume-for-job-interview/src/components/Skills/skills.js
@@ -0,0 +1,24 @@
+import './styles.css';
+function Skills(){
+    return(<>
+        <div className="container-skills">
+            <h1 className="topic-education">Skills</h1>
+
+            <div className="content-skills">
+                <h2 className="topic">HARD SKILL</h2>
+
+                <h3 className="topic-skill">Languages</h3>
+                <h3 className="languages">Python, JAVA, C/C++, JavaScript, TypeScript, PHP, HTML, CSS, SQL</h3>
+
+                <h3 className="topic-skill">Frameworks</h3>
+                <h3 className="languages">Angular, React, Node.js</h3>
+
+                <h3 className="topic-skill">Tools</h3>
+                <h3 className="languages">Draw.io, Adobe XD, Figma, Adobe Photoshop, Trello, Jira</h3>
+
+            </div>
+        </div>
+    </>)
+}
+
+export default Skills;
