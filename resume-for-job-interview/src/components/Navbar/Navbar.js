61e9cd0c3c79207a6db903d58938c79f7b744f6c
Wannarat <wannarat.boonwong@gmail.com>
2022-10-01 02:44:17 +0700

    add-navbar-mobile-size

--- resume-for-job-interview/src/components/Navbar/Navbar.js
+++ resume-for-job-interview/src/components/Navbar/Navbar.js
@@ -1,20 +1,78 @@
 import './styles.css'
+import { FaBars, FaTimes, FaHome } from 'react-icons/fa'
+import { useState } from 'react'
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <>
       <nav className="nav">
-        <div className="container-nav">
-          <div className="site-title-left">
-            <a href="/">Wannarat</a>
+        {/*<div className="container-nav">*/}
+        {/*  <div className="site-title-left">*/}
+        {/*    <a href="/">Wannarat</a>*/}
+        {/*  </div>*/}
+        {/*  <div className="site-title-right">*/}
+        {/*    <a href="/">Home</a>*/}
+        {/*    <a href="/">Education</a>*/}
+        {/*    <a href="/">Experiences</a>*/}
+        {/*    <a href="/">Skills</a>*/}
+        {/*    <a href="/">Contact Me</a>*/}
+        {/*  </div>*/}
+        {/*  <div className="icon">*/}
+        {/*    <a href="/">*/}
+        {/*      <FaBars />*/}
+        {/*    </a>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+
+        <div className="container-nav-mobile">
+          <div className="container-nav">
+            <div className="site-title-left">
+              <a href="/">Wannarat</a>
+            </div>
+            <div className="icon">
+              <a href="/">
+                <FaBars />
+              </a>
+            </div>
           </div>
-          <div className="site-title-right">
+
+          <div className="row">
             <a href="/">Home</a>
             <a href="/">Education</a>
             <a href="/">Experiences</a>
             <a href="/">Skills</a>
             <a href="/">Contact Me</a>
           </div>
+
+          {/*<div className="row-1">*/}
+          {/*  <div className="icon-nav">*/}
+          {/*    <FaHome />*/}
+          {/*    <a href="/">Home</a>*/}
+          {/*  </div>*/}
+
+          {/*  <div className="icon-nav">*/}
+          {/*    <FaHome />*/}
+          {/*    <a href="/">Education</a>*/}
+          {/*  </div>*/}
+
+          {/*  <div className="icon-nav">*/}
+          {/*    <FaHome />*/}
+          {/*    <a href="/">Experiences</a>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
+
+          {/*<div className="row-1">*/}
+          {/*  <div className="icon-nav">*/}
+          {/*    <FaHome />*/}
+          {/*    <a href="/">Skills</a>*/}
+          {/*  </div>*/}
+
+          {/*  <div className="icon-nav">*/}
+          {/*    <FaHome />*/}
+          {/*    <a href="/">Contact Me</a>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
         </div>
       </nav>
     </>
