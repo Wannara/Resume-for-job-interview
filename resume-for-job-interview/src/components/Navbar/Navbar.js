d1ff1c540b00667c19f5305817abe475fff6ff39
Wannarat <wannarat.boonwong@gmail.com>
2022-10-01 23:41:05 +0700

    add-onclick-hamberg

--- resume-for-job-interview/src/components/Navbar/Navbar.js
+++ resume-for-job-interview/src/components/Navbar/Navbar.js
@@ -1,48 +1,56 @@
 import './styles.css'
-import { FaBars, FaTimes, FaHome } from 'react-icons/fa'
+import { FaBars, FaHome, FaUserGraduate } from 'react-icons/fa'
 import { useState } from 'react'
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
+
   return (
     <>
       <nav className="nav">
-        {/*<div className="container-nav">*/}
-        {/*  <div className="site-title-left">*/}
-        {/*    <a href="/">Wannarat</a>*/}
-        {/*  </div>*/}
-        {/*  <div className="site-title-right">*/}
-        {/*    <a href="/">Home</a>*/}
-        {/*    <a href="/">Education</a>*/}
-        {/*    <a href="/">Experiences</a>*/}
-        {/*    <a href="/">Skills</a>*/}
-        {/*    <a href="/">Contact Me</a>*/}
-        {/*  </div>*/}
-        {/*  <div className="icon">*/}
-        {/*    <a href="/">*/}
-        {/*      <FaBars />*/}
-        {/*    </a>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
-
-        <div className="container-nav-mobile">
-          <div className="container-nav">
-            <div className="site-title-left">
-              <a href="/">Wannarat</a>
-            </div>
-            <div className="icon">
-              <a href="/">
-                <FaBars />
-              </a>
-            </div>
+        <div className="container-nav">
+          <div className="site-title-left">
+            <a href="/">Wannarat</a>
           </div>
-
-          <div className="row">
+          <div className="site-title-right">
             <a href="/">Home</a>
             <a href="/">Education</a>
             <a href="/">Experiences</a>
             <a href="/">Skills</a>
             <a href="/">Contact Me</a>
+          </div>
+          <div
+            className="icon"
+            onClick={() => {
+              setIsOpen(!isOpen)
+            }}
+          >
+            <FaBars />
+          </div>
+        </div>
+
+        <div className="container-nav-mobile">
+          <div className={!isOpen ? 'row non-active' : 'row'}>
+            <a href="/">
+              <FaHome />
+              Home
+            </a>
+            <a href="/">
+              <FaUserGraduate />
+              Education
+            </a>
+            <a href="/">
+              <FaUserGraduate />
+              Experiences
+            </a>
+            <a href="/">
+              <FaUserGraduate />
+              Skills
+            </a>
+            <a href="/">
+              <FaUserGraduate />
+              Contact Me
+            </a>
           </div>
 
           {/*<div className="row-1">*/}
