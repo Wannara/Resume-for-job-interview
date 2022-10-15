c5386d5a94c669874a165ae887768290f25cf95e
Wannarat <wannarat.boonwong@gmail.com>
2022-10-15 23:31:43 +0700

    restructure-time-line

--- resume-for-job-interview/src/components/Education/education.js
+++ resume-for-job-interview/src/components/Education/education.js
@@ -6,67 +6,124 @@ function Education() {
       <div id="education" className="container-education">
         <h1 className="topic-education">Education</h1>
         <div className="content-education">
-          <div className="year-education">
-            <h3 className="text-year">
-              2017 - 2021 <br />
-              Chiang Mai University
-              <br />
-              GPAX 3.21
-            </h3>
-            <h3 className="text-year">
-              2013 - 2015 <br />
-              Fakkwan Wittayakom School
-              <br />
-              GPAX 3.86
-            </h3>
+          <div className="time-line">
+            <div className="text-content">
+              <h3 className="text-year">
+                2017 - 2021 <br />
+                Chiang Mai University
+                <br />
+                GPAX 3.21
+              </h3>
+            </div>
+
+            <div className="pipe">
+              <div className="circle"></div>
+              <div className="line"></div>
+            </div>
+
+            <div className="text-content">
+              <h3 className="text-study">
+                Bachelor of Science in <span className="white-space">Computer Science </span>
+                <br />
+                Minor in Data Science
+              </h3>
+            </div>
           </div>
 
-          <div className="line">
-            <span className="circle"></span>
-            <span className="vertical"></span>
-            <span className="circle"></span>
-            <span className="vertical"></span>
+          <div className="time-line">
+            <div className="text-content">
+              <h3 className="text-year">
+                2013 - 2015 <br />
+                Fakkwan Wittayakom School
+                <br />
+                GPAX 3.86
+              </h3>
+            </div>
+
+            <div className="pipe">
+              <div className="circle"></div>
+              <div className="line"></div>
+            </div>
+
+            <div className="text-content">
+              <h3 className="text-study">Science - Mathematics Program</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="education-mobile">
+          <div className="time-line">
+            <div className="pipe">
+              <div className="circle"></div>
+              <div className="line"></div>
+            </div>
+
+            <div className="text-content">
+              <h3 className="text-year">
+                2017 - 2021 <br />
+                Chiang Mai University
+                <br />
+                GPAX 3.21
+              </h3>
+
+              <h3 className="text-study">
+                Bachelor of Science in <span className="white-space">Computer Science </span>
+                <br />
+                Minor in Data Science
+              </h3>
+            </div>
           </div>
 
-          <div className="program-study">
-            <h3 className="text-study">
-              Bachelor of Science in{' '}
-              <span className="white-space">Computer Science </span>
-              <br />
-              Minor in Data Science
-            </h3>
-            <h3 className="text-study">Science - Mathematics Program</h3>
+          <div className="time-line">
+            <div className="pipe">
+              <div className="circle"></div>
+              <div className="line"></div>
+            </div>
+
+            <div className="text-content">
+              <h3 className="text-year">
+                2017 - 2021 <br />
+                Chiang Mai University
+                <br />
+                GPAX 3.21
+              </h3>
+
+              <h3 className="text-study">
+                Bachelor of Science in <span className="white-space">Computer Science </span>
+                <br />
+                Minor in Data Science
+              </h3>
+            </div>
           </div>
         </div>
-        <div className="education-moblie">
-          <div className="line">
-            <span className="circle"></span>
-            <span className="vertical"></span>
-            <span className="circle"></span>
-            <span className="vertical"></span>
-          </div>
-          <div className="year-education">
-            <h3 className="text-year">
-              2017 - 2021 <br />
-              Chiang Mai University
-              <br />
-              GPAX 3.21
-            </h3>
-            <h3 className="text-study">
-              Bachelor of Science in{' '}
-              <span className="white-space">Computer Science </span>
-              <br />
-              Minor in Data Science
-            </h3>
-            <h3 className="text-year">
-              2013 - 2015 <br />
-              Fakkwan Wittayakom School
-              <br />
-              GPAX 3.86
-            </h3>
-            <h3 className="text-study">Science - Mathematics Program</h3>
-          </div>
-        </div>
+        {/*<div className="education-moblie">*/}
+        {/*  <div className="line">*/}
+        {/*    <span className="circle"></span>*/}
+        {/*    <span className="vertical"></span>*/}
+        {/*    <span className="circle"></span>*/}
+        {/*    <span className="vertical"></span>*/}
+        {/*  </div>*/}
+        {/*  <div className="year-education">*/}
+        {/*    <h3 className="text-year">*/}
+        {/*      2017 - 2021 <br />*/}
+        {/*      Chiang Mai University*/}
+        {/*      <br />*/}
+        {/*      GPAX 3.21*/}
+        {/*    </h3>*/}
+        {/*    <h3 className="text-study">*/}
+        {/*      Bachelor of Science in <span className="white-space">Computer Science </span>*/}
+        {/*      <br />*/}
+        {/*      Minor in Data Science*/}
+        {/*    </h3>*/}
+        {/*    <h3 className="text-year">*/}
+        {/*      2013 - 2015 <br />*/}
+        {/*      Fakkwan Wittayakom School*/}
+        {/*      <br />*/}
+        {/*      GPAX 3.86*/}
+        {/*    </h3>*/}
+        {/*    <h3 className="text-study">Science - Mathematics Program</h3>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
       </div>
     </>
   )
--- resume-for-job-interview/src/components/Experiences/experiences.js
+++ resume-for-job-interview/src/components/Experiences/experiences.js
@@ -5,87 +5,121 @@ function Experiences() {
       <div id="experiences" className="container-education">
         <h1 className="topic-education">Experiences</h1>
         <div className="content-education">
-          <div className="year-education">
-            <h3 className="text-year-experiences">
-              June - August 2022 <br />
-              Developer
-            </h3>
-            <h3 className="text-year-experiences">
-              June - Octorber 2021 <br />
-              Cooperative education
-              <br />
-            </h3>
-            <h3 className="text-year-experiences">
-              May - June 2021 <br />
-              Internship
-              <br />
-            </h3>
-          </div>
-
-          <div className="line">
-            <span className="circle"></span>
-            <span className="vertical"></span>
-            <span className="circle"></span>
-            <span className="vertical"></span>
-            <span className="circle"></span>
-            <span className="vertical"></span>
-          </div>
-
-          <div className="program-study">
-            <h3 className="text-study">
-              Tradition Asia <br />
-              Junior Web Developer
-            </h3>
-            <h3 className="text-study">
-              Manao Software Co., Ltd. <br />
-              Junior Web Developer
-            </h3>
-            <h3 className="text-study">
-              Manao Software Co., Ltd. <br />
-              Junior Web Developer
-            </h3>
-          </div>
-        </div>
-
-        <div className="education-moblie">
-          <div className="line">
-            <span className="circle"></span>
-            <span className="vertical"></span>
-            <span className="circle"></span>
-            <span className="vertical"></span>
-            <span className="circle"></span>
-            <span className="vertical"></span>
-          </div>
-          <div className="year-education">
-            <div className="group-ex">
-              <h3 className="text-year-experiences">
+          <div className="time-line">
+            <div className="text-content">
+              <h3 className="text-year">
                 June - August 2022 <br />
                 Developer
               </h3>
+            </div>
+
+            <div className="pipe">
+              <div className="circle"></div>
+              <div className="line"></div>
+            </div>
+
+            <div className="text-content">
               <h3 className="text-study">
                 Tradition Asia <br />
                 Junior Web Developer
               </h3>
             </div>
+          </div>
 
-            <div className="group-ex">
-              <h3 className="text-year-experiences">
+          <div className="time-line">
+            <div className="text-content">
+              <h3 className="text-year">
                 June - Octorber 2021 <br />
                 Cooperative education
-                <br />
               </h3>
+            </div>
+
+            <div className="pipe">
+              <div className="circle"></div>
+              <div className="line"></div>
+            </div>
+
+            <div className="text-content">
               <h3 className="text-study">
                 Manao Software Co., Ltd. <br />
                 Junior Web Developer
               </h3>
             </div>
+          </div>
 
-            <div className="group-ex">
-              <h3 className="text-year-experiences">
+          <div className="time-line">
+            <div className="text-content">
+              <h3 className="text-year">
                 May - June 2021 <br />
                 Internship
-                <br />
               </h3>
+            </div>
+
+            <div className="pipe">
+              <div className="circle"></div>
+              <div className="line"></div>
+            </div>
+
+            <div className="text-content">
+              <h3 className="text-study">
+                Manao Software Co., Ltd. <br />
+                Junior Web Developer
+              </h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="education-mobile">
+          <div className="time-line">
+            <div className="pipe">
+              <div className="circle"></div>
+              <div className="line"></div>
+            </div>
+
+            <div className="text-content">
+              <h3 className="text-year">
+                June - August 2022 <br />
+                Developer
+              </h3>
+
+              <h3 className="text-study">
+                Tradition Asia <br />
+                Junior Web Developer
+              </h3>
+            </div>
+          </div>
+
+          <div className="time-line">
+            <div className="pipe">
+              <div className="circle"></div>
+              <div className="line"></div>
+            </div>
+
+            <div className="text-content">
+              <h3 className="text-year">
+                June - Octorber 2021 <br />
+                Cooperative education
+              </h3>
+
+              <h3 className="text-study">
+                Manao Software Co., Ltd. <br />
+                Junior Web Developer
+              </h3>
+            </div>
+          </div>
+
+          <div className="time-line">
+            <div className="pipe">
+              <div className="circle"></div>
+              <div className="line"></div>
+            </div>
+
+            <div className="text-content">
+              <h3 className="text-year">
+                May - June 2021 <br />
+                Internship
+              </h3>
+
               <h3 className="text-study">
                 Manao Software Co., Ltd. <br />
                 Junior Web Developer
