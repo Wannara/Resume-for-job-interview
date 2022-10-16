14a60359656419660364735a14662507d21655cb
Wannarat <wannarat.boonwong@gmail.com>
2022-10-16 16:21:43 +0700

    update-css

--- resume-for-job-interview/src/components/ContactMe/contact-me.js
+++ resume-for-job-interview/src/components/ContactMe/contact-me.js
@@ -5,26 +5,28 @@ function ContactMe() {
   return (
     <div id="ContactMe" className={styles.containerContact}>
       <h1 className={styles.topicContact}>Contact Me</h1>
-      <div className={styles.contentContact}>
-        <div className={styles.contact}>
-          <div className={styles.picture}>
-            <FaEnvelope size={30} />
+      <div className={styles.contentContactMe}>
+        <div className={styles.contentContact}>
+          <div className={styles.contact}>
+            <div className={styles.picture}>
+              <FaEnvelope size={30} />
+            </div>
+            <h3 className={styles.textContact}>wannarat.boonwong@gmail.com</h3>
           </div>
-          <h3 className={styles.textContact}>wannarat.boonwong@gmail.com</h3>
-        </div>
 
-        <div className={styles.contact}>
-          <div className={styles.picture}>
-            <FaPhoneAlt size={30} />
+          <div className={styles.contact}>
+            <div className={styles.picture}>
+              <FaPhoneAlt size={30} />
+            </div>
+            <h3 className={styles.textContact}>061-3426-963</h3>
           </div>
-          <h3 className={styles.textContact}>061-3426-963</h3>
-        </div>
 
-        <div className={styles.contact}>
-          <div className={styles.picture}>
-            <FaGithubSquare size={30} />
+          <div className={styles.contact}>
+            <div className={styles.picture}>
+              <FaGithubSquare size={30} />
+            </div>
+            <h3 className={styles.textContact}>github.com/Wannara</h3>
           </div>
-          <h3 className={styles.textContact}>github.com/Wannara</h3>
         </div>
       </div>
     </div>
